2aaa05ccdf3c6f649c5b578945fc508d1baa6c8f
Fridolin Pokorny <fridolin@redhat.com>
2018-09-26 19:10:46 +0200

    State configured index in the report message

--- thoth/adviser/python/project.py
+++ thoth/adviser/python/project.py
@@ -370,7 +370,8 @@ class Project:
                     'type': 'INFO',
                     'id': 'ARTIFACT-POSSIBLE-DIFFERENT-SOURCE',
                     'justification': f'Artifacts can be installed from different sources '
-                                     f'({",".join(other_sources.keys())}) not respecting configuration',
+                                     f'({",".join(other_sources.keys())}) not respecting configuration '
+                                     f'that expects {package_version.index!r}',
                     'source': source,
                     'package_locked': package_version.to_pipfile_lock(),
                     'package_name': package_version.name,
