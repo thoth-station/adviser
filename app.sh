80702026e82722e741b365ddc903087343be1cbc
Fridolin Pokorny <fridolin@redhat.com>
2018-10-04 13:34:52 +0200

    Introduce Dependency Monkey template

--- app.sh
+++ app.sh
@@ -7,6 +7,8 @@
 
 if [ "$THOTH_PROVENANCE" == "1" ]; then
 	exec /opt/app-root/bin/python3 thoth-adviser provenance
+if [ "$THOTH_DEPENDENCY_MONKEY" == "1" ]; then
+	exec /opt/app-root/bin/python3 thoth-adviser dependency-monkey
 else
 	exec /opt/app-root/bin/python3 thoth-adviser pypi
 fi
