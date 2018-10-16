fa3cb4109735f7864a7cfeffb08d8d49d65925eb
Fridolin Pokorny <fridolin@redhat.com>
2018-10-16 17:36:24 +0200

    Check for latest packages present on indexes

--- tests/python/test_project.py
+++ tests/python/test_project.py
@@ -18,7 +18,9 @@
 import os
 from base import AdviserTestCase
 
+import attr
 import pytest
+import semantic_version as semver
 
 from thoth.adviser.python import Project
 from thoth.adviser.python import Source
@@ -81,3 +83,93 @@ class TestProject(AdviserTestCase):
 
         assert source.name in project.pipfile_lock.meta.sources
         assert source is project.pipfile_lock.meta.sources[source.name]
+
+    def test_get_outdated_package_versions_indirect(self):
+        # The difference between direct and indirect - Pipenv states "index" in
+        # the Pipfile.lock file if the given package is a direct dependency.
+        # The "index" key for indirect dependencies is omitted though. This way
+        # we check both - logic for indirect/direct is slightly different.
+        # We cannot use flexmock as Source has slots.
+        @attr.s
+        class MySource:
+            url = attr.ib(type=str)
+            verify_ssl = attr.ib(type=bool)
+            name = attr.ib(type=str)
+            warehouse = attr.ib(type=bool, default=False)
+            warehouse_api_url = attr.ib(default=None, type=str)
+
+            def get_latest_package_version(_, package_name):
+                return {
+                    "certifi": semver.Version.coerce("2018.10.15"), 
+                    "chardet": semver.Version.coerce("3.0.4"),
+                    "idna": semver.Version.coerce("2.10"),  # Bumped from 2.7
+                    "requests": semver.Version.coerce("2.19.1"),
+                    "termcolor": semver.Version.coerce("1.1.0"),
+                    "urllib3": semver.Version.coerce("1.23"),
+                }[package_name]
+
+        project = Project.from_files(
+            os.path.join(self.data_dir, 'Pipfile_test2'),
+            os.path.join(self.data_dir, 'Pipfile_test2.lock')
+        )
+
+        new_sources = {}
+        for source in project.pipfile_lock.meta.sources.values():
+            new_sources[source.name] = MySource(**source.to_dict())
+        project.pipfile_lock.meta.sources = new_sources
+
+        for package_version in project.iter_dependencies_locked(with_devel=True):
+            if package_version.index:
+                package_version.index = new_sources[package_version.index.name]
+
+        result = project.get_outdated_package_versions()
+        assert len(result) == 1
+        assert 'idna' in result
+        assert len(result['idna']) == 2
+        assert result['idna'][0] is project.pipfile_lock.packages['idna']
+        assert isinstance(result['idna'][1], semver.Version)
+        assert str(result['idna'][1]) == '2.10.0'
+
+
+    def test_get_outdated_package_versions_direct(self):
+        # See previous test comments for more info.
+        # We cannot use flexmock as Source has slots.
+        @attr.s
+        class MySource:
+            url = attr.ib(type=str)
+            verify_ssl = attr.ib(type=bool)
+            name = attr.ib(type=str)
+            warehouse = attr.ib(type=bool, default=False)
+            warehouse_api_url = attr.ib(default=None, type=str)
+
+            def get_latest_package_version(_, package_name):
+                return {
+                    "certifi": semver.Version.coerce("2018.10.15"), 
+                    "chardet": semver.Version.coerce("3.0.4"),
+                    "idna": semver.Version.coerce("2.7"),
+                    "requests": semver.Version.coerce("3.0.0"),
+                    "termcolor": semver.Version.coerce("1.1.0"),
+                    "urllib3": semver.Version.coerce("1.23"),
+                }[package_name]
+
+        project = Project.from_files(
+            os.path.join(self.data_dir, 'Pipfile_test2'),
+            os.path.join(self.data_dir, 'Pipfile_test2.lock')
+        )
+
+        new_sources = {}
+        for source in project.pipfile_lock.meta.sources.values():
+            new_sources[source.name] = MySource(**source.to_dict())
+        project.pipfile_lock.meta.sources = new_sources
+
+        for package_version in project.iter_dependencies_locked(with_devel=True):
+            if package_version.index:
+                package_version.index = new_sources[package_version.index.name]
+
+        result = project.get_outdated_package_versions()
+        assert len(result) == 1
+        assert 'requests' in result
+        assert len(result['requests']) == 2
+        assert result['requests'][0] is project.pipfile_lock.packages['requests']
+        assert isinstance(result['requests'][1], semver.Version)
+        assert str(result['requests'][1]) == '3.0.0'
