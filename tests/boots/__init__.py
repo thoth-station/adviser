dc47ca18ea0070b09a7c148f257027f3e8b03567
Fridolin Pokorny <fridolin@redhat.com>
2019-12-11 09:44:02 +0100

    Implement version clash boot

--- tests/boots/__init__.py
+++ tests/boots/__init__.py
@@ -15,8 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Boot units implemented in adviser."""
-
-from .version_clash import VersionClashBoot
-
-__all__ = ["VersionClashBoot"]
+"""Tests for a boot pipeline unit implementation."""
