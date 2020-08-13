a2c3badbb35a01b5c4b0789cb230a30fd79faa24
Kevin Postlethwait <k.postlethwait24@gmail.com>
2020-08-13 13:36:31 -0400

    include for stable recommendation and parameterize weights

--- thoth/adviser/steps/security_indicators.py
+++ thoth/adviser/steps/security_indicators.py
@@ -24,6 +24,7 @@ from typing import List
 from typing import Dict
 from typing import TYPE_CHECKING
 import logging
+import os
 
 from thoth.python import PackageVersion
 
@@ -43,20 +44,30 @@ _LOGGER = logging.getLogger(__name__)
 class SecurityIndicatorStep(Step):
     """A step that scores a state based on security info aggregated."""
 
+    _JUSTIFICATION_ADDITION = [
+        {"type": "WARNING", "message": ("Package has no gathered information regarding security."),}
+    ]
+
     # Weights for Confidence
-    HIGH_CONFIDENCE_WEIGHT = 1
-    MEDIUM_CONFIDENCE_WEIGHT = 0.5
-    LOW_CONFIDENCE_WEIGHT = 0.1
+    HIGH_CONFIDENCE_WEIGHT = float(os.environ.get("SI_STEP_HIGH_CONFIDENCE_WEIGHT", 1))
+    MEDIUM_CONFIDENCE_WEIGHT = float(os.environ.get("SI_STEP_MEDIUM_CONFIDENCE_WEIGHT", 0.5))
+    LOW_CONFIDENCE_WEIGHT = float(os.environ.get("SI_STEP_LOW_CONFIDENCE_WEIGHT", 0.1))
 
     # Weights for Security
-    HIGH_SEVERITY_WEIGHT = 100
-    MEDIUM_SEVERITY_WEIGHT = 10
-    LOW_SEVERITY_WEIGHT = 1
+    HIGH_SEVERITY_WEIGHT = float(os.environ.get("SI_STEP_HIGH_SEVERITY_WEIGHT", 100))
+    MEDIUM_SEVERITY_WEIGHT = float(os.environ.get("SI_STEP_MEDIUM_SEVERITY_WEIGHT", 10))
+    LOW_SEVERITY_WEIGHT = float(os.environ.get("SI_STEP_LOW_SEVERITY_WEIGHT", 1))
+
+    # Overall weight for security rewards
+    SI_REWARD_WEIGHT = float(os.environ.get("SI_STEP_REWARD_WEIGHT", 0.5))
 
     @classmethod
     def should_include(cls, builder_context: "PipelineBuilderContext") -> Optional[Dict[str, Any]]:
         """Register only if we are explicitly recommending secure stacks."""
-        if builder_context.recommendation_type == RecommendationType.SECURE:
+        if (
+            builder_context.recommendation_type == RecommendationType.SECURE
+            or builder_context.recommendation_type == RecommendationType.STABLE
+        ):
             return {}
         else:
             return None
@@ -65,21 +76,19 @@ class SecurityIndicatorStep(Step):
         self, state: State, package_version: PackageVersion
     ) -> Optional[Tuple[Optional[float], Optional[List[Dict[str, str]]]]]:
         """Score package based on security indicators gathered, do not include if not analyzed."""
-        s_info = self.context.graph.si_aggregated_python_package_version_exists(
-            package_name=package_version.name,
-            package_version=package_version.locked_version,
-            index_url=package_version.index,
-        ) and self.context.graph.get_si_aggregated_python_package_version(
+        s_info = self.context.graph.get_si_aggregated_python_package_version(
             package_name=package_version.name,
             package_version=package_version.locked_version,
             index_url=package_version.index,
         )
 
-        if not s_info:
-            raise NotAcceptable(
-                f"No security info for {package_version.name}==={package_version.locked_version} "
-                f"on {package_version.index}"
-            )
+        if s_info is None:
+            if self.context.recommendation_type == RecommendationType.SECURE:
+                raise NotAcceptable(
+                    f"No security info for {package_version.name}==={package_version.locked_version} "
+                    f"on {package_version.index}"
+                )
+            return 0, self._JUSTIFICATION_ADDITION
 
         s_score = (
             (
@@ -103,5 +112,6 @@ class SecurityIndicatorStep(Step):
         )
 
         s_score = s_score / s_info["number_of_lines_with_code_in_python_files"]
-
-        return min(1, 1 / s_score), None
+        if s_score < 1:
+            return self.SI_REWARD_WEIGHT, None
+        return self.SI_REWARD_WEIGHT / s_score, None
