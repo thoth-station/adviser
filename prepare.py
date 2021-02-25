d0c68612de874502e03139ea63ed817a2f942279
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2021-02-25 21:46:22 +0100

    Remove bits related to preparation

--- prepare.py
+++ prepare.py
@@ -16,14 +16,10 @@ _REQUEST_FILE_DIR = os.getenv("THOTH_ADVISER_REQUEST_FILE_PATH", "/mnt/workdir/"
 _ADVISER_WORKDIR = os.getenv("THOTH_ADVISER_WORKDIR", "/opt/app-root/src")
 _ADVISER_INPUTS_DIR = os.path.join(_ADVISER_WORKDIR, "input")
 _ADVISER_SUBCOMMAND = os.getenv("THOTH_ADVISER_SUBCOMMAND")
-_ENABLE_PREPARE = bool(int(os.getenv("THOTH_ADVISER_ENABLE_PREPARE", "0")))
 
 
 def main() -> None:
     """Prepare input files for adviser."""
-    if not _ENABLE_PREPARE:
-        return
-
     os.makedirs(_ADVISER_INPUTS_DIR, exist_ok=False)
 
     if not _DOCUMENT_ID:
