44545a030ef3103550653c484ed96ca0128694fb
Kebechet <noreply+kebechet@redhat.com>
2021-06-14 11:35:41 +0000

    Release of version 0.33.2

--- thoth/adviser/__init__.py
+++ thoth/adviser/__init__.py
@@ -40,7 +40,7 @@ from .unit import Unit
 from .wrap import Wrap
 
 __title__ = "thoth-adviser"
-__version__ = "0.33.1"
+__version__ = "0.33.2"
 __author__ = "Fridolin Pokorny <fridolin@redhat.com>"
 
 
