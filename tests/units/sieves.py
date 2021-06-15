24cd1087edca8521d1e99af17a70ac0311e68b37
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2021-06-15 10:37:05 +0200

    Adjust copyright notice in headers

--- tests/units/sieves.py
+++ tests/units/sieves.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # thoth-adviser
-# Copyright(C) 2019, 2020 Fridolin Pokorny
+# Copyright(C) 2019 - 2021 Fridolin Pokorny
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
