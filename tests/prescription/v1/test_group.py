2b983889c86789d7fde245c19f1f89a109a70b29
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2021-11-07 21:37:07 +0100

    Implement group step prescription

---
+++ tests/prescription/v1/test_group.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python3
+# thoth-adviser
+# Copyright(C) 2021 Fridolin Pokorny
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Test implementation of group step."""
+
+import yaml
+
+from thoth.adviser.pipeline_builder import PipelineBuilderContext
+from thoth.adviser.prescription.v1 import GroupStepPrescription
+from thoth.adviser.prescription.v1.schema import PRESCRIPTION_GROUP_STEP_SCHEMA
+
+from .base import AdviserUnitPrescriptionTestCase
+
+
+class TestGroupStepPrescription(AdviserUnitPrescriptionTestCase):
+    """Test implementation of group step.
+
+    As the run implementation is shared with raw step, only computation done in should_include is tested here.
+    """
+
+    def test_should_include(self, builder_context: PipelineBuilderContext) -> None:
+        """Test calculating configuration for the given prescription unit."""
+        prescription_str = f"""\
+name: GroupStep
+type: step.Group
+should_include:
+  adviser_pipeline: true
+match:
+  group:
+  - package_version:
+      name: numpy
+      version: '!=1.21.4'
+      index_url:
+        not: 'https://pypi.org/simple'
+  - package_version:
+      name: tensorflow
+      version: '~=2.7.0'
+  - package_version:
+      name: pandas
+      index_url: 'https://thoth-station.ninja'
+      develop: true
+  - package_version:
+      name: werkzeug
+      version: '==1.0.0'
+      index_url: 'https://pypi.org/simple'
+      develop: false
+run:
+  stack_info:
+  - type: INFO
+    message: Running the unit.
+    link: 'https://thoth-station.ninja'
+"""
+        prescription = yaml.safe_load(prescription_str)
+        PRESCRIPTION_GROUP_STEP_SCHEMA(prescription)
+        GroupStepPrescription.set_prescription(prescription)
+
+        assert list(GroupStepPrescription.should_include(builder_context)) == [
+            {
+                "match": {
+                    "package_version": {
+                        "index_url": {"not": "https://pypi.org/simple"},
+                        "name": "numpy",
+                        "version": "!=1.21.4",
+                    },
+                    "state": {
+                        "resolved_dependencies": [
+                            {"name": "tensorflow", "version": "~=2.7.0"},
+                            {"develop": True, "index_url": "https://thoth-station.ninja", "name": "pandas"},
+                            {
+                                "develop": False,
+                                "index_url": "https://pypi.org/simple",
+                                "name": "werkzeug",
+                                "version": "==1.0.0",
+                            },
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "numpy",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+            {
+                "match": {
+                    "package_version": {"name": "tensorflow", "version": "~=2.7.0"},
+                    "state": {
+                        "resolved_dependencies": [
+                            {"index_url": {"not": "https://pypi.org/simple"}, "name": "numpy", "version": "!=1.21.4"},
+                            {"develop": True, "index_url": "https://thoth-station.ninja", "name": "pandas"},
+                            {
+                                "develop": False,
+                                "index_url": "https://pypi.org/simple",
+                                "name": "werkzeug",
+                                "version": "==1.0.0",
+                            },
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "tensorflow",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+            {
+                "match": {
+                    "package_version": {"develop": True, "index_url": "https://thoth-station.ninja", "name": "pandas"},
+                    "state": {
+                        "resolved_dependencies": [
+                            {"index_url": {"not": "https://pypi.org/simple"}, "name": "numpy", "version": "!=1.21.4"},
+                            {"name": "tensorflow", "version": "~=2.7.0"},
+                            {
+                                "develop": False,
+                                "index_url": "https://pypi.org/simple",
+                                "name": "werkzeug",
+                                "version": "==1.0.0",
+                            },
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "pandas",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+            {
+                "match": {
+                    "package_version": {
+                        "develop": False,
+                        "index_url": "https://pypi.org/simple",
+                        "name": "werkzeug",
+                        "version": "==1.0.0",
+                    },
+                    "state": {
+                        "resolved_dependencies": [
+                            {"index_url": {"not": "https://pypi.org/simple"}, "name": "numpy", "version": "!=1.21.4"},
+                            {"name": "tensorflow", "version": "~=2.7.0"},
+                            {"develop": True, "index_url": "https://thoth-station.ninja", "name": "pandas"},
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "werkzeug",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+        ]
+
+    def test_should_include_groups(self, builder_context: PipelineBuilderContext) -> None:
+        """Test calculating configuration for the given prescription unit, multiple groups."""
+        prescription_str = f"""\
+name: GroupStep
+type: step.Group
+should_include:
+  adviser_pipeline: true
+match:
+- group:
+  - package_version:
+      name: numpy
+      version: '!=1.21.4'
+      index_url:
+        not: 'https://pypi.org/simple'
+  - package_version:
+      name: tensorflow
+      version: '~=2.7.0'
+- group:
+  - package_version:
+      name: pandas
+      index_url: 'https://thoth-station.ninja'
+      develop: true
+  - package_version:
+      name: werkzeug
+      version: '==1.0.0'
+      index_url: 'https://pypi.org/simple'
+      develop: false
+run:
+  stack_info:
+  - type: INFO
+    message: Running the unit.
+    link: 'https://thoth-station.ninja'
+"""
+        prescription = yaml.safe_load(prescription_str)
+        PRESCRIPTION_GROUP_STEP_SCHEMA(prescription)
+        GroupStepPrescription.set_prescription(prescription)
+
+        assert list(GroupStepPrescription.should_include(builder_context)) == [
+            {
+                "match": {
+                    "package_version": {
+                        "index_url": {"not": "https://pypi.org/simple"},
+                        "name": "numpy",
+                        "version": "!=1.21.4",
+                    },
+                    "state": {"resolved_dependencies": [{"name": "tensorflow", "version": "~=2.7.0"}]},
+                },
+                "multi_package_resolution": True,
+                "package_name": "numpy",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+            {
+                "match": {
+                    "package_version": {"name": "tensorflow", "version": "~=2.7.0"},
+                    "state": {
+                        "resolved_dependencies": [
+                            {"index_url": {"not": "https://pypi.org/simple"}, "name": "numpy", "version": "!=1.21.4"}
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "tensorflow",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+            {
+                "match": {
+                    "package_version": {"develop": True, "index_url": "https://thoth-station.ninja", "name": "pandas"},
+                    "state": {
+                        "resolved_dependencies": [
+                            {
+                                "develop": False,
+                                "index_url": "https://pypi.org/simple",
+                                "name": "werkzeug",
+                                "version": "==1.0.0",
+                            }
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "pandas",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+            {
+                "match": {
+                    "package_version": {
+                        "develop": False,
+                        "index_url": "https://pypi.org/simple",
+                        "name": "werkzeug",
+                        "version": "==1.0.0",
+                    },
+                    "state": {
+                        "resolved_dependencies": [
+                            {"develop": True, "index_url": "https://thoth-station.ninja", "name": "pandas"}
+                        ]
+                    },
+                },
+                "multi_package_resolution": True,
+                "package_name": "werkzeug",
+                "prescription": {"run": False},
+                "run": {
+                    "stack_info": [
+                        {"link": "https://thoth-station.ninja", "message": "Running the unit.", "type": "INFO"}
+                    ]
+                },
+            },
+        ]
---
+++ thoth/adviser/prescription/v1/group.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# thoth-adviser
+# Copyright(C) 2021 Fridolin Pokorny
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""A base class for implementing group steps."""
+
+import attr
+from typing import Any
+from typing import Dict
+from typing import Generator
+from typing import Optional
+import logging
+
+from voluptuous import Schema
+from voluptuous import Required
+
+from packaging.specifiers import SpecifierSet
+from .schema import PRESCRIPTION_GROUP_STEP_MATCH_SCHEMA
+from .schema import PRESCRIPTION_GROUP_STEP_RUN_SCHEMA
+from .step import StepPrescription
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@attr.s(slots=True)
+class GroupStepPrescription(StepPrescription):
+    """A base class for implementing group steps.
+
+    The declarative interface triggers registration of multiple pipeline units for all the package versions
+    stated. All the pipeline units have shared context to exchange shared information (ex. if stack_info was
+    already added and so). This is an optimization to make sure the pipeline unit is run only if needed (based
+    on package_name).
+    """
+
+    CONFIGURATION_SCHEMA: Schema = Schema(
+        {
+            Required("package_name"): str,
+            Required("match"): PRESCRIPTION_GROUP_STEP_MATCH_SCHEMA,
+            Required("multi_package_resolution"): True,  # Required to match multiple packages.
+            Required("run"): PRESCRIPTION_GROUP_STEP_RUN_SCHEMA,
+            Required("prescription"): Schema({"run": bool}),
+        }
+    )
+
+    _specifier = attr.ib(type=Optional[SpecifierSet], kw_only=True, init=False, default=None)
+    _index_url = attr.ib(type=Optional[str], kw_only=True, init=False, default=None)
+    _develop = attr.ib(type=Optional[bool], kw_only=True, init=False, default=None)
+
+    @staticmethod
+    def _yield_should_include(unit_prescription: Dict[str, Any]) -> Generator[Dict[str, Any], None, None]:
+        """Yield for every entry stated in the match field."""
+        match = unit_prescription["match"]
+        run = unit_prescription["run"]
+        prescription_conf = {"run": False}
+
+        # The trick here is to construct resolved dependencies for all the combinations that can happen. If the
+        # group step states two package versions named - "foo", "bar", there are created two units:
+        #   - the first one matches "foo" and resolved dependencies state "bar"
+        #   - the second one matches "bar" and resolved dependencies state "foo"
+        # Similarly, if there are multiple packages stated.
+        for match_entry in match if isinstance(match, list) else [match]:
+            for idx, item in enumerate(match_entry["group"]):
+                resolved_dependencies = []
+                for other_idx, other_item in enumerate(match_entry["group"]):
+                    if idx != other_idx:
+                        resolved_dependencies.append(other_item["package_version"])
+
+                package_version_dict = item["package_version"]
+                yield {
+                    "package_name": package_version_dict["name"],
+                    "multi_package_resolution": True,
+                    "match": {
+                        "package_version": package_version_dict,
+                        "state": {
+                            "resolved_dependencies": resolved_dependencies,
+                        },
+                    },
+                    "run": run,
+                    "prescription": prescription_conf,
+                }
